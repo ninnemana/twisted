8383182a0d9bb6bb30ee9f151ddc5602a1983b50
Alex Ninneman <alex@ninneman.org>
2014-02-08 14:56:38 -0600

    switched tabs

--- public/js/controllers/events.js
+++ public/js/controllers/events.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('twisted').controller('EventsController', ['$scope', 'Global', function ($scope, Global) {
-    $scope.global = Global;
+	$scope.global = Global;
 }]);
